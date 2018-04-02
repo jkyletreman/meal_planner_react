cd95758816ee6525dc8bc72ccaa4abccc13cece5
Kyle Treman <jkt290@nyu.edu>
2018-04-02 12:55:58 -0400

    Added Router Nav between pages
    Changed all React Router action to be contained within the Header Component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import Feed from './components/js/Feed';
 import Header from './components/js/Header';
-import WeekView from './components/js/WeekView';
 
 function App() {
   return (
     <React.Fragment>
       <Header/>
-      <WeekView />
-      {/* <Feed/> */}
+
     </React.Fragment>
   );
 }
--- src/components/js/Header.jsx
+++ src/components/js/Header.jsx
@@ -1,35 +1,48 @@
 import React from 'react';
 import { Menu, Icon } from 'antd';
-const SubMenu = Menu.SubMenu;
-const MenuItemGroup = Menu.ItemGroup;
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Feed from './Feed';
+import WeekView from './WeekView';
+
 
 class Header extends React.Component {
   state = {
-    current: 'mail',
+    current: 'bank'
   }
-  handleClick = (e) => {
-    console.log('click ', e);
-    this.setState({
-      current: e.key,
-    });
-  }
+  // handleClick = (e) => {
+  //   console.log('click ', e);
+  //   this.setState({current: e.key});
+  // }
   render() {
     return (
-      <Menu
-        onClick={this.handleClick}
-        selectedKeys={[this.state.current]}
-        mode="horizontal"
-      >
-        <Menu.Item key="bank">
-          <Icon type="bank" />Feed
-        </Menu.Item>
-        <Menu.Item key="shake" >
-          <Icon type="shake" />Weekly
-        </Menu.Item>
-        <Menu.Item key="About">
-          <a href="https://ant.design" target="_blank" rel="noopener noreferrer">About</a>
-        </Menu.Item>
-      </Menu>
+      <Router>
+        <React.Fragment>
+          <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
+
+            <Menu.Item key="bank">
+              <Link to='/'>
+                <Icon type="bank"/>Feed
+              </Link>
+            </Menu.Item>
+
+            <Menu.Item key="shake">
+              <Link to='/week'>
+                <Icon type="shake"/>Weekly
+              </Link>
+            </Menu.Item>
+
+            <Menu.Item key="mail">
+              <a href="https://ant.design" target="_blank" rel="noopener noreferrer">About</a>
+            </Menu.Item>
+          </Menu>
+
+        <Switch>
+          <Route exact path="/" render={() => <Feed />} />
+          <Route exact path="/week" render={() => <WeekView />} />
+        </Switch>
+        
+      </React.Fragment>
+    </Router>
     );
   }
 }
