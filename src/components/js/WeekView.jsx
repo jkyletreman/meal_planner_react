123626d551b14353744f0b4ee001385cb8437e96
Kyle Treman <jkt290@nyu.edu>
2018-04-03 11:46:10 -0400

    Fix views

--- src/components/js/WeekView.jsx
+++ src/components/js/WeekView.jsx
@@ -1,33 +1,27 @@
 import React from 'react';
 import { Divider } from 'antd';
 
-const WeekView = (props) => {
-    const { Monday } = props.week
-    return Monday[0] === undefined ? <p>'No Meals Selected...'</p> : { Monday.map(recipe => <p>recipe</p>) }
-};
 // const WeekView = (props) => {
-//   let {
-//     Monday,
-//     Tuesday,
-//     Wednesday,
-//     Thursday,
-//     Friday,
-//   } = props.week
-//   return (
-//     <div>
-//       <Divider dashed />
-//       <h2>Your Weekly Dashboard</h2>
-//       <Divider orientation="left">Monday</Divider>
-//
-//       <Divider orientation="left">Tuesday</Divider>
-//       <p>No Meals Selected...</p>
-//       <Divider orientation="left">Wednesday</Divider>
-//       <p>No Meals Selected...</p>
-//       <Divider orientation="left">Thursday</Divider>
-//       <p>No Meals Selected...</p>
-//       <Divider orientation="left">Friday</Divider>
-//       <p>No Meals Selected...</p>
-//       <Divider dashed />
-//     </div>
-// )};
+//     const { Monday } = props.week
+//     return Monday[0] === undefined ? <p>'No Meals Selected...'</p> : <p>{Monday[0].name}</p>
+// };
+const WeekView = (props) =>
+    <div>
+      <Divider dashed />
+      <h2>Your Weekly Dashboard</h2>
+      {Object.entries(props.week).map(([day, recipes]) =>
+        <Day name={day} recipes={recipes}/>
+      )}
+    </div>
+
+
+const Day = ({name, recipes=[]}) =>
+  <div>
+    <Divider orientation="left">{name}</Divider>
+    <div>
+    {recipes.map(recipe =>
+      <p>{recipe.name}</p>
+    )}</div>
+  </div>
+
 export default WeekView;
