76944cd62042399811eb25482fb253a50561bc2b
Kyle Treman <jkt290@nyu.edu>
2018-04-03 11:45:55 -0400

    Refractor

--- src/components/js/Header.jsx
+++ src/components/js/Header.jsx
@@ -37,9 +37,7 @@ class Header extends React.Component {
   handleChange = (value) => {
     this.setState({ day: value })
   }
-  handleSubmission = () => {
 
-  }
   updateDay(info) {
     const day = this.state.day;
 
