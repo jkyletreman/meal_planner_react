1dade458f5e6e4f9a44eb1f47b76f0d702dad646
Kyle Treman <jkt290@nyu.edu>
2018-04-03 14:04:45 -0400

    Add function to pull ids

--- src/components/js/IngredientsList.jsx
+++ src/components/js/IngredientsList.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
 
-const IngredientsList = () =>
-  <p>Hi</p>
+const IngredientsList = (props) => {
+  const ids = [] || 'null';
+  const arrayArrayObject = Object.values(props.week);
+
+  for (var i = 0; i < arrayArrayObject.length; i++) {
+    var innerArray = arrayArrayObject[i];
+
+    for (var j = 0; j < innerArray.length; j++) {
+      var innerObject = innerArray[j];
+      var id = innerObject.id;
+      ids.push(id);
+    }
+  }
+  
+  return <p>{ids}</p>
+};
 
 export default IngredientsList;
