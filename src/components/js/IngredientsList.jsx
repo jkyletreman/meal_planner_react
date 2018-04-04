45df69240fbad4265a699ab2e7537367f9ca12d6
Kyle Treman <jkt290@nyu.edu>
2018-04-04 12:22:45 -0400

    Converted to class component

--- src/components/js/IngredientsList.jsx
+++ src/components/js/IngredientsList.jsx
@@ -1,20 +1,50 @@
-import React from 'react';
+import React, { Component } from "react";
 
-const IngredientsList = (props) => {
-  const ids = [] || 'null';
-  const arrayArrayObject = Object.values(props.week);
-
-  for (var i = 0; i < arrayArrayObject.length; i++) {
-    var innerArray = arrayArrayObject[i];
-
-    for (var j = 0; j < innerArray.length; j++) {
-      var innerObject = innerArray[j];
-      var id = innerObject.id;
-      ids.push(id);
-    }
+export default class IngredientsList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ids: []
+    };
+    this.getIngredients = this.getIngredients.bind(this);
+    this.setIds = this.setIds.bind(this);
   }
-  
-  return <p>{ids}</p>
-};
 
-export default IngredientsList;
+  async getIngredients() {
+    const request = await fetch("/api/ingredients", {
+      method: "GET",
+      qs: '1 2 3'
+    });
+    const response = request.json();
+  }
+
+  componentWillMount() {
+    const ingredients = this.getIngredients() || null;
+    console.log(ingredients);
+  }
+
+  setIds() {
+    const ids = [] || "null";
+    const arrayArrayObject = Object.values(this.props.week);
+
+    for (var i = 0; i < arrayArrayObject.length; i++) {
+      var innerArray = arrayArrayObject[i];
+
+      for (var j = 0; j < innerArray.length; j++) {
+        var innerObject = innerArray[j];
+        var id = innerObject.id;
+      }
+    }
+    this.setState({ ids: id });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.ids.map(id => {
+          <p>{id}</p>;
+        })}
+      </div>
+    );
+  }
+}
