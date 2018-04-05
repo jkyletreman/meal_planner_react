22484736e1fe6cb733a15546b7ac9b34c0839dd7
Kyle Treman <jkt290@nyu.edu>
2018-04-05 10:27:35 -0400

    Fix api calls with setState callbacks

--- src/components/js/IngredientsList.jsx
+++ src/components/js/IngredientsList.jsx
@@ -4,25 +4,27 @@ export default class IngredientsList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      ids: [1, 2, 3],
-      ingredients: []
-      }
+      ids: [],
+      ingredients: [],
+      mealsSelected: false
+    };
     this.getIngredients = this.getIngredients.bind(this);
     this.setIds = this.setIds.bind(this);
+    this.toIngredientsObject = this.toIngredientsObject.bind(this);
   }
 
   async getIngredients() {
-    const request = await fetch(`/api/ingredients?ids=${this.state.ids}`)
+    const request = await fetch(`/api/ingredients?ids=${this.state.ids}`);
     const response = await request.json();
-    this.setState({ingredients: response})
+    this.setState({ ingredients: response });
   }
 
   componentWillMount() {
-    this.getIngredients();
+    this.setIds();
   }
 
   setIds() {
-    const ids = [] || "null";
+    const ids = [];
     const arrayArrayObject = Object.values(this.props.week);
 
     for (var i = 0; i < arrayArrayObject.length; i++) {
@@ -31,18 +33,66 @@ export default class IngredientsList extends Component {
       for (var j = 0; j < innerArray.length; j++) {
         var innerObject = innerArray[j];
         var id = innerObject.id;
+        ids.push(id);
       }
     }
-    this.setState({ ids: id });
+    // because setState is async need to pass conditionals as callbacks
+    this.setState({ ids: ids }, () => {
+      const ids = this.state.ids;
+
+      if (ids.length > 0) {
+        this.setState({ mealsSelected: true }, () => {
+          if (this.state.mealsSelected) {
+            this.getIngredients();
+          }
+        });
+      }
+    });
+  }
+
+  toIngredientsObject(array) {
+    const newObj = {};
+
+    array.map(object => {
+      const name = object.name;
+      const id = object.id;
+      const amount = object.amount;
+      const unit = object.unit;
+
+      if (!newObj[name]) {
+        newObj[name] = {
+          id,
+          amount,
+          unit
+        };
+      } else {
+        newObj[name].amount += amount;
+      }
+    });
+    return newObj;
   }
 
   render() {
+    const ingredientObject = this.toIngredientsObject(this.state.ingredients);
+    const ingredientsToDisplay = Object.entries(ingredientObject);
+    console.log(ingredientsToDisplay);
+    // when mapped over position 0 will be the name and position 1 will be the data
     return (
-      <div>
-        {this.state.ingredients.map(ingredient =>
-          <p key={ingredient.id}>{ingredient.name}</p>
+      <React.Fragment>
+        {this.state.mealsSelected ? (
+          ingredientsToDisplay.map(ingredient => {
+            return (
+              <div key={ingredient[1].id}>
+                <p>{ingredient[0]}</p>
+                <p>{ingredient[1].amount}</p>
+                <p>{ingredient[1].unit}</p>
+              </div>
+            );
+          })
+        ) : (
+          <p>No Meals Selected</p>
         )}
-      </div>
+      </React.Fragment>
     );
   }
 }
