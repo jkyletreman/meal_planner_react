be0867f3b788516ed014a8b3d8c5a0c8c065f35a
Kyle Treman <jkt290@nyu.edu>
2018-04-10 11:02:08 -0400

    Created style objects

--- src/components/js/WeekView.jsx
+++ src/components/js/WeekView.jsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import { Divider } from 'antd';
-import "../css/WeekView.css"
+import React from "react";
+import { Divider } from "antd";
+import "../css/WeekView.css";
 
-const WeekView = (props) =>
-    <div>
-      <h2 style={{margin: "0 auto", textAlign: "center", fontSize: '30px'}}>Your Weekly Dashboard</h2>
-      <Divider dashed />
-      {Object.entries(props.week).map(([day, recipes]) =>
-        <Day name={day} recipes={recipes}/>
-      )}
-    </div>
+const headerStyle = {
+  margin: "0 auto",
+  textAlign: "center",
+  fontSize: "30px"
+};
 
+const textStyle = {
+  fontSize: "20px",
+  color: "#ED5147",
+  letterSpacing: "2px"
+};
 
-const Day = ({name, recipes=[]}) =>
+const WeekView = props => (
   <div>
-    <Divider style={{fontSize: '16px'}} orientation="left">{name}</Divider>
-    <div>
-    {recipes.map(recipe =>
-      <p>{recipe.name}</p>
-    )}</div>
+    <h2 style={headerStyle}>Your Weekly Dashboard</h2>
+    <Divider dashed />
+    {Object.entries(props.week).map(([day, recipes]) => (
+      <Day name={day} recipes={recipes} />
+    ))}
   </div>
+);
+
+const Day = ({ name, recipes = [] }) => (
+  <div>
+    <Divider orientation="left">{name}</Divider>
+    <div>{recipes.map(recipe => <p style={textStyle}>{recipe.name}</p>)}</div>
+  </div>
+);
 
 export default WeekView;
