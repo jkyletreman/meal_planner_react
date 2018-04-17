183f874dc9906501e51d0b29a90f1e8950b009bb
Kyle Treman <jkt290@nyu.edu>
2018-04-17 18:53:04 -0400

    Refractored App/Header
    Took the majority of the functionality and moved it to the App component, leaving Header as the actual Header. The day switch is working, currently fixing the card adding feature.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,111 @@
-import React from 'react';
-import Header from './components/js/Header';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Header from "./components/js/Header";
+import Feed from "./components/js/Feed";
+import WeekView from "./components/js/WeekView";
+import IngredientsList from "./components/js/IngredientsList";
 
-function App() {
-  return (
-    <React.Fragment>
-      <Header/>
-    </React.Fragment>
-  );
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      day: "Monday",
+      info: [],
+      week: {
+        Monday: [],
+        Tuesday: [],
+        Wednesday: [],
+        Thursday: [],
+        Friday: []
+      }
+    };
+    this.setDay = this.setDay.bind(this)
+  }
+
+  componentWillMount() {
+    this.getCards();
+  }
+  async getCards() {
+    // https://feedme-node-api.herokuapp.com/
+    const res = await fetch("api/smallCards");
+    const cards = await res.json();
+    this.setState({ info: cards });
+  }
+
+  setDay(value) {
+    this.setState({ day: value });
+  };
+
+  updateDay(info) {
+    const day = this.state.day;
+
+    switch (this.state.day) {
+      case "Monday":
+        const monday = this.state.week.Monday;
+        monday.push(info);
+        this.setState((this.state.week.Monday = monday));
+        break;
+
+      case "Tuesday":
+        const tuesday = this.state.week.Tuesday;
+        tuesday.push(info);
+        this.setState((this.state.week.Tuesday = tuesday));
+        break;
+
+      case "Wednesday":
+        const wednesday = this.state.week.Wednesday;
+        wednesday.push(info);
+        this.setState((this.state.week.Wednesday = wednesday));
+        break;
+
+      case "Thursday":
+        const thursday = this.state.week.Thursday;
+        thursday.push(info);
+        this.setState((this.state.week.Thursday = thursday));
+        break;
+
+      case "Friday":
+        const friday = this.state.week.Friday;
+        friday.push(info);
+        this.setState((this.state.week.Friday = friday));
+        break;
+
+      default:
+        console.log("nope");
+    }
+  }
+  render() {
+    return (
+      <Router>
+        <React.Fragment>
+          <Header
+            setDay={this.setDay}
+          />
+          <Switch>
+            <Route
+              exact="exact"
+              path="/"
+              render={() => (
+                <Feed
+                  updateDay={this.updateDay}
+                  info={this.state.info}
+                  day={this.state.day}
+                />
+              )}
+            />
+            <Route
+              exact="exact"
+              path="/week"
+              render={() => <WeekView week={this.state.week} />}
+            />
+            <Route
+              exact="exact"
+              path="/list"
+              render={() => <IngredientsList week={this.state.week} />}
+            />
+          </Switch>
+        </React.Fragment>
+      </Router>
+    );
+  }
 }
-
-export default App;
--- src/components/js/Header.jsx
+++ src/components/js/Header.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { Menu, Icon, Select, Button } from "antd";
+import { Menu, Icon, Select, Button, Layout } from "antd";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import Feed from "./Feed";
-import WeekView from "./WeekView";
-import IngredientsList from "./IngredientsList";
-import "../css/Header.css"
+
+import TestNav from "./TestNav";
+import "../css/Header.css";
 const Option = Select.Option;
 
 const spacing = {
@@ -12,91 +11,32 @@ const spacing = {
   margin: "0 auto",
   justifyContent: "center",
   backgroundColor: "#ED5147",
-  fontSize: '1.6em',
+  fontSize: "1.6em"
 };
 
 const textColor = {
   color: "white",
   letterSpacing: "1px",
   fontWeight: "200"
-}
+};
 
-class Header extends React.Component {
+export default class Header extends React.Component {
   constructor() {
     super();
     this.state = {
       current: "",
-      day: "Monday",
-      info: [],
-      week: {
-        Monday: [],
-        Tuesday: [],
-        Wednesday: [],
-        Thursday: [],
-        Friday: []
-      }
     };
-    this.updateDay = this.updateDay.bind(this);
     this.handleClick = this.handleClick.bind(this);
-  }
-
-  componentWillMount() {
-    this.getCards();
-  }
-  async getCards() {
-    // https://feedme-node-api.herokuapp.com/
-    const res = await fetch("api/smallCards");
-    const cards = await res.json();
-    this.setState({ info: cards });
   }
   handleClick = e => {
     this.setState({ current: e.key });
   };
-  handleChange = value => {
-    this.setState({ day: value });
+  handleChange = (value) => {
+    this.props.setDay(value);
   };
-
-  updateDay(info) {
-    const day = this.state.day;
-
-    switch (day) {
-      case "Monday":
-        const monday = this.state.week.Monday;
-        monday.push(info);
-        this.setState((this.state.week.Monday = monday));
-        break;
-
-      case "Tuesday":
-        const tuesday = this.state.week.Tuesday;
-        tuesday.push(info);
-        this.setState((this.state.week.Tuesday = tuesday));
-        break;
-
-      case "Wednesday":
-        const wednesday = this.state.week.Wednesday;
-        wednesday.push(info);
-        this.setState((this.state.week.Wednesday = wednesday));
-        break;
-
-      case "Thursday":
-        const thursday = this.state.week.Thursday;
-        thursday.push(info);
-        this.setState((this.state.week.Thursday = thursday));
-        break;
-
-      case "Friday":
-        const friday = this.state.week.Friday;
-        friday.push(info);
-        this.setState((this.state.week.Friday = friday));
-        break;
-
-      default:
-        console.log("nope");
-    }
-  }
   render() {
     return (
-      <Router>
+      <React.Fragment>
         <div className="main-container">
           <Menu
             onClick={this.handleClick}
@@ -149,33 +89,8 @@ class Header extends React.Component {
               </Select>
             </Menu.Item>
           </Menu>
-
-          <Switch>
-            <Route
-              exact="exact"
-              path="/"
-              render={() => (
-                <Feed
-                  updateDay={this.updateDay}
-                  info={this.state.info}
-                  day={this.state.day}
-                />
-              )}
-            />
-            <Route
-              exact="exact"
-              path="/week"
-              render={() => <WeekView week={this.state.week} />}
-            />
-            <Route
-              exact="exact"
-              path="/list"
-              render={() => <IngredientsList week={this.state.week} />}
-            />
-          </Switch>
         </div>
-      </Router>
+      </React.Fragment>
     );
   }
 }
-export default Header;
