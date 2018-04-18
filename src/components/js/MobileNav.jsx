74e64c24aa85649f78521238c154654e27a2df0b
Kyle Treman <jkt290@nyu.edu>
2018-04-17 23:25:33 -0400

    Prettier

--- src/components/js/MobileNav.jsx
+++ src/components/js/MobileNav.jsx
@@ -1,59 +1,57 @@
-import React, { Component } from 'react'
-import { slide as Menu } from 'react-burger-menu'
+import React, { Component } from "react";
+import { slide as Menu } from "react-burger-menu";
 import { Link } from "react-router-dom";
-import { Icon, Select } from "antd"
-import "../css/MobileNav.css"
-
+import { Icon, Select } from "antd";
+import "../css/MobileNav.css";
 
 const Option = Select.Option;
 
 export default class MobileNav extends React.Component {
-  showSettings (event) {
+  showSettings(event) {
     event.preventDefault();
   }
 
   constructor() {
-    super()
+    super();
     this.state = {
-      current: "",
+      current: ""
     };
-  };
+  }
 
-  handleChange = (value) => {
+  handleChange = value => {
     this.props.setDayForRecipes(value);
   };
 
-  render () {
+  render() {
     return (
       <div>
+        <Menu>
+          <Link to="/">
+            <Icon type="dashboard" />Feed ME!
+          </Link>
 
+          <Link to="/week">
+            <Icon type="menu-unfold" />Weekly
+          </Link>
 
-      <Menu>
-        <Link to="/">
-          <Icon type="dashboard" />Feed ME!
-        </Link>
-
-        <Link to="/week">
-          <Icon type="menu-unfold" />Weekly
-        </Link>
-
-        <Link to="/list">
-          <Icon type="file-text" />Shopping List
-        </Link>
-      </Menu>
-      <Select
-        defaultValue="Select a Day"
-        style={{
-          width: 200, float: 'right'
-        }}
-        onChange={this.handleChange}
-      >
-        <Option value="Monday">Monday</Option>
-        <Option value="Tuesday">Tuesday</Option>
-        <Option value="Wednesday">Wednesday</Option>
-        <Option value="Thursday">Thursday</Option>
-        <Option value="Friday">Friday</Option>
-      </Select>
+          <Link to="/list">
+            <Icon type="file-text" />Shopping List
+          </Link>
+        </Menu>
+        <Select
+          defaultValue="Select a Day"
+          style={{
+            width: 200,
+            float: "right"
+          }}
+          onChange={this.handleChange}
+        >
+          <Option value="Monday">Monday</Option>
+          <Option value="Tuesday">Tuesday</Option>
+          <Option value="Wednesday">Wednesday</Option>
+          <Option value="Thursday">Thursday</Option>
+          <Option value="Friday">Friday</Option>
+        </Select>
       </div>
     );
   }
