65ef06fdea27dac3d60b187b5eecc4b3c4fddcc4
Kyle Treman <jkt290@nyu.edu>
2018-04-18 21:05:10 -0400

    Passing id to Popover as props

--- src/components/js/FoodCard.jsx
+++ src/components/js/FoodCard.jsx
@@ -1,40 +1,46 @@
 import React from "react";
 import { Card, Avatar, message, Icon } from "antd";
+import Popup from "./Popup"
 import "antd/dist/antd.css";
-import "../css/FoodCard.css"
+import "../css/FoodCard.css";
 
 const { Meta } = Card;
 
 export default class FoodCard extends React.Component {
   constructor(props) {
     super(props);
-    this.handleClick = this.handleClick.bind(this);
+    this.handleClickAdd = this.handleClickAdd.bind(this);
   }
-  handleClick() {
+  handleClickAdd() {
     const info = this.props.info;
     this.props.updateDayWithRecipe(info);
     message.info(`Added to ${this.props.day}`);
   }
-
   render() {
     return (
       <React.Fragment>
-          <Card
-            className="feed-food-card"
-            hoverable
-            style={{boxShadow: "1.5px 1.5px 1.5px black"}}
-
-            actions={[<Icon onClick={this.handleClick} type="plus-circle-o" />, <Icon type="info-circle-o" />]}
-            cover={
-              <img className="feed-food-card-img" alt="example" src={this.props.info.img} />
-            }
-          >
-            <Meta
-              avatar={<Avatar src="https://image.ibb.co/eML677/Profile.jpg" />}
-              title={this.props.info.name}
-              description={this.props.info.summary}
+        <Card
+          className="feed-food-card"
+          hoverable
+          style={{ boxShadow: "1.5px 1.5px 1.5px black" }}
+          actions={[
+            <Icon onClick={this.handleClickAdd} type="plus-circle-o" />,
+            <Popup recipeId={this.props.info.id}/>
+          ]}
+          cover={
+            <img
+              className="feed-food-card-img"
+              alt="example"
+              src={this.props.info.img}
             />
-          </Card>
+          }
+        >
+          <Meta
+            avatar={<Avatar src="https://image.ibb.co/eML677/Profile.jpg" />}
+            title={this.props.info.name}
+            description={this.props.info.summary}
+          />
+        </Card>
       </React.Fragment>
     );
   }
